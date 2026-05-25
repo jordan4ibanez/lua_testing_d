bb91c1d9e48e8e9c4833c74df4e99e42779883d9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-05-24 22:36:41 -0400

    Interesting

--- source/app.d
+++ source/app.d
@@ -1,27 +1,16 @@
 import bindbc.lua;
 import std.stdio;
+import std.string;
 
 void main() {
-	writeln("Edit source/app.d to start your project.");
+	lua_State* lua = luaL_newstate();
 
-	// This version attempts to load the lua shared library using well-known variations
-	// of the library name for the host system.
-	LuaSupport ret = loadLua();
-	if (ret != luaSupport) {
+	luaL_openlibs(lua);
 
-		// Handle error. For most use cases, its reasonable to use the the error handling API in
-		// bindbc-loader to retrieve error messages for logging and then abort. If necessary, it's
-		// possible to determine the root cause via the return value:
+	luaL_dostring(lua, `
+		print("hello world")
+		if type(jit) == "table" then print("luajit!! " .. jit.version) end
+	`.toStringz());
 
-		if (ret == luaSupport.noLibrary) {
-			// Lua shared library failed to load
-		} else if (luaSupport.badLibrary) {
-			// One or more symbols failed to load. The likely cause is that the
-			// shared library is a version different from the one the app was
-			// configured to load
-		}
-	} else {
-		writeln("success!");
-	}
-
+	lua_close(lua);
 }
